a8443d439e81ccf5fcd615eccc68f88a73823db2
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-09-29 11:37:37 +0000

    Item11153: Forgot to bump release version
    git-svn-id: http://svn.foswiki.org/trunk/FilterPlugin@12647 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/FilterPlugin.pm
+++ lib/Foswiki/Plugins/FilterPlugin.pm
@@ -25,7 +25,7 @@ use vars qw(
     );
 
 $VERSION = '$Rev$';
-$RELEASE = '2.06';
+$RELEASE = '2.07';
 $NO_PREFS_IN_TOPIC = 1;
 $SHORTDESCRIPTION = 'Substitute and extract information from content by using regular expressions';
 
